ea940b9b69b3e4456731b3f1ed09529a18d2c81a
Patrick Long <patrick.long@gmail.com>
2021-08-14 15:08:34 -0700

    add stack

---
+++ data_structures.py
@@ -0,0 +1,23 @@
+"""Basic data structures"
+
+class Stack:
+    def __init__(self):
+         self.items = []
+
+    def  checkEmpty(self):
+        return self.items == []
+
+    def  push(self, item):
+        self.items.append(item)
+
+    def  pop (self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items(self.size -1]
+
+    def size(self):
+        return  len(self.items)
+
+
+
